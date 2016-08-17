854045652eb4b2e22a8f8f537bb90ce6c6f98eeb
Jacob Paine <painejacob@gmail.com>
2016-08-17 09:06:57 -0500

    Initial form build

--- public/js/app.js
+++ public/js/app.js
@@ -87,33 +87,33 @@ angular.module("contactsApp", ['ngRoute'])
             });
         }
     })
-    .controller("EditContactController", function($scope, $routeParams, Contacts) {
-        Contacts.getContact($routeParams.contactId).then(function(doc) {
-            $scope.contact = doc.data;
-        }, function(response) {
-            alert(response);
-        });
-
-        $scope.toggleEdit = function() {
-            $scope.editMode = true;
-            $scope.contactFormUrl = "contact-form.html";
-        }
-
-        $scope.back = function() {
-            $scope.editMode = false;
-            $scope.contactFormUrl = "";
-        }
-
-        $scope.saveContact = function(contact) {
-            Contacts.editContact(contact);
-            $scope.editMode = false;
-            $scope.contactFormUrl = "";
-        }
-
-        $scope.deleteContact = function(contactId) {
-            Contacts.deleteContact(contactId);
-        }
-    })
+    // .controller("EditContactController", function($scope, $routeParams, Contacts) {
+    //     Contacts.getContact($routeParams.contactId).then(function(doc) {
+    //         $scope.contact = doc.data;
+    //     }, function(response) {
+    //         alert(response);
+    //     });
+    //
+    //     $scope.toggleEdit = function() {
+    //         $scope.editMode = true;
+    //         $scope.contactFormUrl = "contact-form.html";
+    //     }
+    //
+    //     $scope.back = function() {
+    //         $scope.editMode = false;
+    //         $scope.contactFormUrl = "";
+    //     }
+    //
+    //     $scope.saveContact = function(contact) {
+    //         Contacts.editContact(contact);
+    //         $scope.editMode = false;
+    //         $scope.contactFormUrl = "";
+    //     }
+    //
+    //     $scope.deleteContact = function(contactId) {
+    //         Contacts.deleteContact(contactId);
+    //     }
+    // })
     .controller("ExampleController", function($scope, $routeParams, Contacts) {
       Contacts.getContact($routeParams.contactId).then(function(doc) {
           $scope.contact = doc.data;
